6b1b9fe70c33a6f3852ee8e952b207c03dc67327
Tavish Pegram <tapegram@gmail.com>
2023-06-07 11:36:28 -0500

    remove old themes

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,12 +1,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.gruvbox" },
-  { import = "astrocommunity.colorscheme.kanagawa"},
-  { import = "astrocommunity.colorscheme.nightfox"},
-  { import = "astrocommunity.colorscheme.rose-pine" },
-  { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   { import = "astrocommunity.editing-support.text-case-nvim" },
