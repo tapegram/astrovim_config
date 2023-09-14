d698365b097d529e10602f806be71c50ec03c6e7
Tavish Pegram <tapegram@gmail.com>
2023-09-14 13:41:51 -0500

    treesitter changes

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,15 +3,4 @@ return {
   opts = {
     -- ensure_installed = { "lua" },
   },
-  lazy = false,
-  config = function()
-    vim.filetype.add { extension = { u = "unison" } }
-    require("nvim-treesitter.parsers").get_parser_configs().unison = { 
-      install_info = {
-        url = "~/projects/tree-sitter-unison",
-        files = { "src/parser.c", "src/scanner.c", "src/maybe.c" },
-        branch = "main",
-      },
-    }
-  end,
 }
